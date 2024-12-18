c19002f1edcf9b49735c56f4cbd806216f834cbf
lazyhat <sevapetrov2016p@gmail.com>
2024-12-19 00:56:21 +0300

    add endpoint and service bean

--- src/main/kotlin/ru/jetlabs/ts/paymentservice/rest/PaymentServiceController.kt
+++ src/main/kotlin/ru/jetlabs/ts/paymentservice/rest/PaymentServiceController.kt
@@ -1,10 +1,22 @@
 package ru.jetlabs.ts.paymentservice.rest
 
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/ts-payment-service")
 class PaymentServiceController {
+    @PostMapping("/agency")
+    fun addAgencyBinding(
+        @RequestBody d: String
+    ) {
 
+    }
 }
+
+data class AddAgencyBindingRequest(
+    val agencyId: Long,
+    val bankAccountNumber: String,
+)
---
+++ src/main/kotlin/ru/jetlabs/ts/paymentservice/service/PaymentService.kt
@@ -0,0 +1,45 @@
+package ru.jetlabs.ts.paymentservice.service
+
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.insert
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
+import ru.jetlabs.ts.paymentservice.PaymentServiceApplication
+import ru.jetlabs.ts.paymentservice.rest.AddAgencyBindingRequest
+import ru.jetlabs.ts.paymentservice.tables.AgencyBankAccountsBindings
+import java.sql.SQLException
+
+sealed interface AddAgencyBindingResult {
+    data object Success : AddAgencyBindingResult
+    sealed interface Error : AddAgencyBindingResult {
+        val message: String
+
+        data class AgencyAlreadyExist(val agencyId: Long) : Error {
+            override val message: String = "Agency already exists(id = $agencyId)"
+        }
+
+        data class Unknown(override val message: String) : Error
+    }
+}
+
+@Component
+@Transactional
+class PaymentService {
+    companion object {
+        val LOGGER = LoggerFactory.getLogger(PaymentServiceApplication::class.java)!!
+    }
+
+    fun addAgencyBinding(addAgencyBindingRequest: AddAgencyBindingRequest): AddAgencyBindingResult = try {
+        AgencyBankAccountsBindings.insert {
+            it[agencyId] = addAgencyBindingRequest.agencyId
+            it[bankAccountNumber] = addAgencyBindingRequest.bankAccountNumber
+        }
+        AddAgencyBindingResult.Success
+    } catch (e: SQLException) {
+        if (AgencyBankAccountsBindings.select(emptyList())
+                .where(AgencyBankAccountsBindings.agencyId eq addAgencyBindingRequest.agencyId).count() >= 1
+        ) AddAgencyBindingResult.Error.AgencyAlreadyExist(addAgencyBindingRequest.agencyId)
+        else AddAgencyBindingResult.Error.Unknown(e.message ?: "id = ${addAgencyBindingRequest.agencyId}")
+    }
+}
