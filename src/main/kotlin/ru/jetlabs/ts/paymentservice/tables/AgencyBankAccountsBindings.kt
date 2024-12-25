f2e53ee44bb4f0dcef398928e6099c983357d36c
Alex <makargravanov@gmail.com>
2024-12-26 02:53:00 +0300

    change bank_account_number to 16 symbols

--- src/main/kotlin/ru/jetlabs/ts/paymentservice/tables/AgencyBankAccountsBindings.kt
+++ src/main/kotlin/ru/jetlabs/ts/paymentservice/tables/AgencyBankAccountsBindings.kt
@@ -4,7 +4,7 @@ import org.jetbrains.exposed.sql.Table
 
 object AgencyBankAccountsBindings : Table("agency_bank_accounts_bindings") {
     val agencyId = long("agency_id").entityId().uniqueIndex()
-    val bankAccountNumber = char("bank_account_number", 20).check { it regexp "^[0-9]{20}$" }
+    val bankAccountNumber = char("bank_account_number", 16).check { it regexp "^[0-9]{16}$" }
 
     override val primaryKey = PrimaryKey(agencyId)
 }
