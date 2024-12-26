7f1dd41941c66667c366e9c409ebb892b3f89495
lazyhat <sevapetrov2016p@gmail.com>
2024-12-26 17:31:41 +0300

    deploy fix

---
+++ src/main/kotlin/ru/jetlabs/ts/paymentservice/db/Database.kt
@@ -0,0 +1,19 @@
+package ru.jetlabs.ts.ticketsservice.db
+
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.springframework.boot.ApplicationArguments
+import org.springframework.boot.ApplicationRunner
+import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
+import ru.jetlabs.ts.paymentservice.tables.AgencyBankAccountsBindings
+import ru.jetlabs.ts.paymentservice.tables.Transactions
+import ru.jetlabs.ts.paymentservice.tables.TransactionsStatuses
+
+@Component
+@Transactional
+class SchemaInitialize : ApplicationRunner {
+
+    override fun run(args: ApplicationArguments?) {
+        SchemaUtils.create(Transactions, TransactionsStatuses, AgencyBankAccountsBindings)
+    }
+}
