8e6cc38b345143cafa27d320083bd6188f795493
lazyhat <sevapetrov2016p@gmail.com>
2024-12-26 17:46:27 +0300

    deploy fix

--- src/main/kotlin/ru/jetlabs/ts/paymentservice/tables/AgencyBankAccountsBindings.kt
+++ src/main/kotlin/ru/jetlabs/ts/paymentservice/tables/AgencyBankAccountsBindings.kt
@@ -3,7 +3,7 @@ package ru.jetlabs.ts.paymentservice.tables
 import org.jetbrains.exposed.dao.id.IdTable
 
 object AgencyBankAccountsBindings : IdTable<Long>("agency_bank_accounts_bindings") {
-    val agencyId = long("agency_id").entityId().uniqueIndex()
+    val agencyId = long("agency_id").uniqueIndex()
     val bankAccountNumber = char("bank_account_number", 16).check { it regexp "^[0-9]{16}$" }
     override val id = Transactions.long("id").entityId().uniqueIndex()
     override val primaryKey = PrimaryKey(id)
