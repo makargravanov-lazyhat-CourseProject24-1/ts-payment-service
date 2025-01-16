2145888f742b408249a9de18f71768dbb081c249
Alex <makargravanov@gmail.com>
2025-01-16 16:21:42 +0300

    try to fix the 404 ticket

--- src/main/kotlin/ru/jetlabs/ts/paymentservice/config/TicketFeignClient.kt
+++ src/main/kotlin/ru/jetlabs/ts/paymentservice/config/TicketFeignClient.kt
@@ -5,7 +5,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PutMapping
 
-@FeignClient(name = "ts-tickets-service", url = "http://ts-tickets-service:8080/ts-tickets-service/api/v1")
+@FeignClient(name = "ts-tickets-service", url = "http://ts-tickets-service:8080/ts-tickets-service/api/v1/tickets")
 interface TicketFeignClient {
     @PutMapping("/{id}/cancel")
     fun cancelTicket(@PathVariable id: Long): ResponseEntity<*>
